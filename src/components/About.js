33e7d9a99975357b68676b3e574d9de56f4424e5
DerekDunlap <derekdunlapjr@gmail.com>
2022-01-07 22:11:32 -0500

    applied styling changes to homepage to improve UI

--- src/components/About.js
+++ src/components/About.js
@@ -24,12 +24,12 @@ function About() {
                     <img id='about-img' src='https://images.unsplash.com/photo-1556118903-08e0b48041fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'></img>
                 </div>
             </div>
-            <div className='container-fluid' id='about-container-2'>
+            <div className='container-fluid' id='about-container-1'>
             <div id='about-img-div'>
                     <img id='about-img' src='https://images.unsplash.com/photo-1556118903-08e0b48041fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'></img>
                 </div>
                 <div id='about-description-div'>
-                    <p id='about-paragraph-2'>
+                    <p id='about-paragraph-1'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
